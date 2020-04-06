0c37c56ba5a77a36d97264349b321f9641ce2c9b
industlabs <57274248+industlabs@users.noreply.github.com>
2020-04-05 19:19:55 -0500

    Update start.sh

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -7,6 +7,15 @@ cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 mkdir -p /data/node-red/user/vis || true
 cp /usr/src/app/vis/* /data/node-red/user/vis
 
+chmod 770 /data/node-red/user/vis
+chmod 770 /data/node-red/user/vis/CAC
+chmod 770 /data/node-red/user/vis/main
+chmod 770 /data/node-red/user/vis/filter1
+chmod 770 /data/node-red/user/vis/CAC/vis-user.css
+chmod 770 /data/node-red/user/vis/CAC/vis-views.json
+chmod 770 /data/node-red/user/vis/main/vis-user.css
+chmod 770 /data/node-red/user/vis/mian/vis-views.json
+
 mkdir -p /data/node-red/user/JsonDB || true
 cp /usr/src/app/JsonDB/* /data/node-red/user/JsonDB
 
