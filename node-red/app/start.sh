80021cc1cec552a4773b870790f0ecbe686f75e2
industlabs <57274248+industlabs@users.noreply.github.com>
2020-04-05 19:17:15 -0500

    Update start.sh

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -4,5 +4,12 @@
 mkdir -p /data/node-red/user/lib/flows || true
 cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 
+mkdir -p /data/node-red/user/vis || true
+cp /usr/src/app/vis/* /data/node-red/user/vis
+
+mkdir -p /data/node-red/user/JsonDB || true
+cp /usr/src/app/JsonDB/* /data/node-red/user/JsonDB
+
+
 # Start app
 DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket node-red --settings /usr/src/app/settings.js
