bab4812a960296a15c37b6f8b798f16386e362b1
industlabs <57274248+industlabs@users.noreply.github.com>
2020-04-05 19:32:20 -0500

    Update start.sh

--- node-red/app/start.sh
+++ node-red/app/start.sh
@@ -4,20 +4,11 @@
 mkdir -p /data/node-red/user/lib/flows || true
 cp /usr/src/app/flows/* /data/node-red/user/lib/flows/
 
-mkdir -p /data/node-red/user/vis || true
-cp /usr/src/app/vis/* /data/node-red/user/vis
+#mkdir -p /data/node-red/user/vis || true
+cp -r /usr/src/app/vis/ /data/node-red/user/
 
-chmod 770 /data/node-red/user/vis
-chmod 770 /data/node-red/user/vis/CAC
-chmod 770 /data/node-red/user/vis/main
-chmod 770 /data/node-red/user/vis/filter1
-chmod 770 /data/node-red/user/vis/CAC/vis-user.css
-chmod 770 /data/node-red/user/vis/CAC/vis-views.json
-chmod 770 /data/node-red/user/vis/main/vis-user.css
-chmod 770 /data/node-red/user/vis/mian/vis-views.json
-
-mkdir -p /data/node-red/user/JsonDB || true
-cp /usr/src/app/JsonDB/* /data/node-red/user/JsonDB
+#mkdir -p /data/node-red/user/JsonDB || true
+cp -r /usr/src/app/JsonDB/ /data/node-red/user/
 
 
 # Start app
