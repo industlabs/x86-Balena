36d5e2ba96b9753cd755f3e3d09275455b4481b2
industlabs <57274248+industlabs@users.noreply.github.com>
2020-04-21 13:34:18 -0500

    Add files via upload

---
+++ virtualhere/start-virtualhere.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+#
+# Script used to setup and run virtualhere
+
+ARCH=`uname -m`
+if [ "$ARCH" = "x86_64" ]
+then
+    echo "Your system architecture was identified as $ARCH. The x86_64 version of the virtualhere-client will be used."
+    ARCH_VR=vhusbdx86_64
+else 
+    echo "Your system architecture was identified as $ARCH. The ARM version of the virtualhere-client will be used."
+    ARCH_VR=vhusbdarm 
+fi
+
+echo '*** Listing all usb-devices. Use this list to adjust AllowedDevices in config.ini ...'
+lsusb || echo 'Execution of command lsusb failed. Make sure you have access to USB-Devices on the host.'
+
+cd data
+
+echo '*** Deleting previous bus_usb_*-files from data directory ...'
+find . -name '*bus_usb_*' -delete
+echo '*** Checking for new version of VirtualHere ...'
+wget -N https://www.virtualhere.com/sites/default/files/usbserver/$ARCH_VR || echo 'Checking for remote copy of VirtualHere failed'
+
+chmod +x ./$ARCH_VR
+./$ARCH_VR
+
+exit 0
